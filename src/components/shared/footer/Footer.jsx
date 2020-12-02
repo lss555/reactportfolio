f369aa5d92fc423431ffbc2a04b657e4879321d9
lss555 <chrisheibel7@gmail.com>
2020-12-02 08:12:38 -0700

    Footer setup

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -5,22 +5,50 @@ import './Footer.css'
 function Footer () {
 
   return(
-  <section>
-    <hr />
-    <section>
-      <a href='https://www.linkedin.com/in/chrisheibel7/' style={{display: "table-cell"}} rel="noreferrer" target="_blank">LinkedIn</a>
-    </section>
-    <section>
+  <section className='footer'>
+    <hr className='footer-seperator'/>
+    <section className='footer-social-media'>
       <section className='footer-info-left'>
+      <section className='footer-info__linkedin'>
+      <a href='https://www.linkedin.com/in/chrisheibel7/'>LinkedIn</a>
+    </section>
+    <section className='footer-info__github'>
+    <a href='https://github.com/lss555'>GitHub</a>
+  </section>
+  <section>
+    ChrisHeibel7@gmail.com
+  </section>
+  <section>
+    +1-719-225-6136
+  </section>
+  <section>
+    Denver, CO
+  </section>
 
       </section>
       <section className='footer-info-center'>
 
       </section>
       <section className='footer-info-right'>
+        <section>
+          Home
+        </section>
+        <section>
+          About
+        </section>
+        <section>
+          All Projects
+        </section>
+        <section>
+          Resume
+        </section>
+        <section>
+          Contact
+        </section>
 
       </section>
     </section>
+    <hr className='footer-seperator'/>
   </section>
   )
 }
