19cdb80c7dfc7d662581ee4297a2df2c9ff782d4
lss555 <chrisheibel7@gmail.com>
2021-01-08 09:30:22 -0700

    Resume image

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,27 +2,30 @@
 import './Resume.scss'
 import React from 'react';
 import pdf from '../photos/resume.pdf'
-import SinglePagePDFViewer from "../pdf/single-page"
-
+import resume from '../photos/resume-1.png'
+// import SinglePagePDFViewer from "../pdf/single-page"
+// <SinglePagePDFViewer className='single-page-resume' pdf={pdf} />
 function Resume() {
   return (
     <div>
-      <div>
-        <SinglePagePDFViewer className='single-page-resume' pdf={pdf} />
-      </div>
-        <div className="center">
-          <div className="download-btn btn-1">
-          <a href='./resume.pdf' download="Chris-Heibel-resume" className='download-link'>
-            <div  className="top">
-               Download
-            </div>
-          </a>
-          <a href={pdf} download="Chris-Heibel-resume" className='download-link'>
-              <div className="bottom">
-                PDF
+      <div className='btn-and-resume'>
+        <div className='resume-div'>
+          <img className='resume-img' src={resume} alt='' />
+        </div>
+          <div className="center">
+            <div className="download-btn btn-1">
+            <a href='./resume.pdf' download="Chris-Heibel-resume" className='download-link'>
+              <div  className="top">
+                 Download
               </div>
-              </a>
-          </div>
+            </a>
+            <a href={pdf} download="Chris-Heibel-resume" className='download-link'>
+                <div className="bottom">
+                  PDF
+                </div>
+                </a>
+              </div>
+            </div>
         </div>
     </div>
   )
